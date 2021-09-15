1c45c0c8668ab55181e61c8e79b7f0ed6b53e848
Sinhraphathatron <81318775+Sinhraphathatron@users.noreply.github.com>
2021-09-15 18:21:12 +0700

    add convert json

--- TestREST/Program.cs
+++ TestREST/Program.cs
@@ -5,22 +5,30 @@ using System.Text;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace TestREST
 {
     class Program
     {
+        public static string json { get; set; }
+
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Test REST API 2gis\r");
+            Console.WriteLine("------------------------\n");
+            Console.WriteLine("Test REST API \n");
             Console.WriteLine("------------------------\n");
 
-            Console.WriteLine("Type the key of the GET method, and then press Enter");
+            Console.WriteLine("Type the key of the GET method, and then press Enter:\n");
             string KeyGet = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
 
-            Console.WriteLine("Type the value of the GET method, and then press Enter");
+            Console.WriteLine("Type the value of the GET method, and then press Enter:\n");
             string ValueGet = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n");
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
                 "https://regions-test.2gis.com/1.0/regions?" + KeyGet + "=" + ValueGet);
@@ -30,15 +38,28 @@ namespace TestREST
             Stream receiveStream = response.GetResponseStream();
 
             StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
-           // string jsonString = JsonSerializer.Serialize(readStream.ReadToEnd());
+
+          
+
+            string json = readStream.ReadToEnd();
+
+            JObject jobject = JObject.Parse(json);
+
+            string exmpl = jobject.ToString();
+
+           
 
             Console.WriteLine("------------------------\n");
-            Console.WriteLine("You got an response");
+            Console.WriteLine("You got a response:\n");
             Console.WriteLine("------------------------\n");
-            //Console.WriteLine(jsonString);
-            Console.WriteLine(readStream.ReadToEnd());
+           
+            Console.WriteLine(exmpl);
+            
             response.Close();
             readStream.Close();
+
+
+
 
         }
     }
